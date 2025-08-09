3a468e2b1ab58063e51f532ee16cb3291cec5957
Najaina <rotsiniainanajaina@gmail.com>
2025-08-09 16:07:37 +0300

    09-08-2025

--- assets/components/RhDashboard.vue
+++ assets/components/RhDashboard.vue
@@ -1,5 +1,13 @@
 <template>
   <nav id="menu-verticale">
+    <div id="menu">
+      <a href="">Tableau de bord</a>
+      <a href="">Feedbacks</a>
+      <a href="">Rapports</a>
+      <a href="">Gestion d'utilisateurs</a>
+    </div>
+  </nav>
+  <nav id="menu-content">
     
   </nav>
 </template>
