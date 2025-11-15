38f015e0ed07c264dbecfda6736db19c349e091f
Najaina <rotsiniainanajaina@gmail.com>
2025-11-15 08:59:12 +0300

    Note

--- backend/src/Repository/NoteRepository.php
+++ backend/src/Repository/NoteRepository.php
@@ -16,13 +16,32 @@ class NoteRepository extends ServiceEntityRepository
         parent::__construct($registry, Note::class);
     }
 
-    public function get_all_notes() {
-        return $this->getEntityManager()->createQuery(
-            'SELECT n, u FROM App\Entity\Note n 
-             LEFT JOIN n.matricule u'
-        )
-        ->getResult();
+    public function get_all_notes(): array 
+    {
+        $cnx = $this->getEntityManager()->getConnection();
+
+        $start = new \DateTime('first day of this month 00:00:00');
+        $end   = new \DateTime('last day of this month 23:59:59');
+
+        $sql = "
+            SELECT 
+                vigie.name,
+                ROUND(AVG(note.note)::numeric, 2) AS moyenne
+            FROM note
+            INNER JOIN users ON note.matricule = users.matricule
+            INNER JOIN vigie ON vigie.id = users.vigie
+            WHERE note.date BETWEEN :start AND :end
+            GROUP BY vigie.id
+            ORDER BY vigie.id
+        ";
+
+        $stmt = $cnx->prepare($sql);
+        $stmt->bindValue('start', $start->format('Y-m-d H:i:s'));
+        $stmt->bindValue('end',   $end->format('Y-m-d H:i:s'));
+
+        return $stmt->executeQuery()->fetchAllAssociative();
     }
+
 
     //    /**
     //     * @return Note[] Returns an array of Note objects
